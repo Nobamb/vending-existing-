8f0ba3a0183aec4d9d62dbb3e5ef2d49f60d4cda
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-09 12:06:49 +0900

    getMoney, inputDetail 가져오고 값 입력받기 및 실행 테스트

---
+++ main.js
@@ -0,0 +1,10 @@
+// getMoney import 받기
+import getMoney from "./input/getMoney/getMoney.js";
+// inputDetail 테스트
+import inputDetail from "./input/inputDetail/inputDetail.js";
+
+
+const input = inputDetail("원하는 값을 입력하시오")
+
+// getMoney 테스트
+getMoney(input)
