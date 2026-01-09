a8c498d9080c5b395c22c51e807e5cdd6cf333a3
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-09 15:03:50 +0900

    자판기 보유 현금 데이터구조 변경

--- data/vending/vendingMoney.js
+++ data/vending/vendingMoney.js
@@ -1,26 +1,40 @@
 // 현금(초기값)
 
 // 자판기가 가지고 있는 현금
-let vendingMoney = {
-  // 50000원 0개
+// 현금 데이터 전체를 배열 및 객체 형식으로
+let vendingMoney = [
   // 5만원권
-  money_50000: 0,
-  // 10000원 0개
+  {
+    name: "50000원",
+    count: 1,
+    value: 50000,
+  },
   // 1만원권
-  money_10000: 0,
-  // 5000원 0개
-  // 5천원권
-  money_5000: 0,
-  // 1000원 0개
-  // 1천원권
-  money_1000: 0,
-  // 500원 20개
-  // 500원
-  money_500: 20,
-  // 100원 0개
-  // 100원
-  money_100: 0,
-};
-
+  {
+    name: "10000원",
+    count: 1,
+    value: 10000,
+  }, // 5천원권
+  {
+    name: "5천원권",
+    count: 1,
+    value: 5000,
+  }, // 1천원권
+  {
+    name: "1000원",
+    count: 1,
+    value: 1000,
+  }, // 500원
+  {
+    name: "500원",
+    count: 1,
+    value: 500,
+  }, // 100원
+  {
+    name: "100원",
+    count: 1,
+    value: 100,
+  },
+];
 // export
 export default vendingMoney
