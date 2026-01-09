776f69a4301eec62806aa02a8c192c4d4c04cdd9
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-09 13:02:29 +0900

    word import함

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 문구 import받기
+import word from "./data/word/word.js";
 // getMoney import 받기
 import getMoney from "./input/getMoney/getMoney.js";
 // inputDetail 테스트
@@ -5,7 +7,7 @@ import inputDetail from "./input/inputDetail/inputDetail.js";
 
 
 // inputDetail 비동기화시켰기에 await 붙이기
-const input = await inputDetail("원하는 값을 입력하시오")
+const input = await inputDetail(word.mainWord)
 
 // getMoney 테스트
 getMoney(input)
