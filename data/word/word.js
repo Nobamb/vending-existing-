b2179fbb0cc52169e7396b165448ab3d6a968f4b
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-09 12:55:06 +0900

    전체 word 지정

---
+++ data/word/word.js
@@ -0,0 +1,13 @@
+// inputDetail에서 지정할 문구들 전부 저장
+
+const word = {
+
+    mainWord : "원하는 값을 입력하시오",
+    mainReWord : "카드는 1번, 현금은 2번을 선택하세요",
+    paymentWord : "지불할 현금을 고르시오\n 1. 50000원 2. 10000원 \n3. 5000원 4. 1000원 5. 500원 6. 100원 7. 뒤로 가기" 
+    
+
+}
+
+// export 
+export default word
