28a9b11297867e0855e637ed292020c8b554ce1d
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-09 14:48:19 +0900

    moneyData 전체 값 출력시 of => in으로 변경(객체에서 배열로 바뀌었으므로)

--- input/func/showAllUserMoney.js
+++ input/func/showAllUserMoney.js
@@ -5,8 +5,8 @@ import moneyData from "../../data/user/moneyData.js"
 const showAllUserMoney = () => {
 
     // moneyData하나씩 출력
-    for(const moneytype of moneyData){
-        // 값을 모두 출력
+    for(const moneytype in moneyData){
+    // 값을 모두 출력
         console.log(moneytype)
 
     }
