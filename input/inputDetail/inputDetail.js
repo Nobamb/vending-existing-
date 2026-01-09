a79c7cf5d43a11e9886fcf0d814c8f719d8e9c5f
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-09 12:42:46 +0900

    inputDetail 수정(비동기화)

--- input/inputDetail/inputDetail.js
+++ input/inputDetail/inputDetail.js
@@ -1,28 +1,39 @@
 // 값을 입력받는 함수(node.js상에서)
 
-import readline from "node:readline";
+import readline from "node:readline/promises";
 
 // inputDetail
 // 입력값으로 문구를 받음
-const inputDetail = (word) => {
+const inputDetail = async (word) => {
     const rl = readline.createInterface({
       // 프로그램의 입력을 받음
       input: process.stdin,
       // 프로그램의 출력을 줌
       output: process.stdout,
     });
-    
-    // 질문을 함(질문하면서 값도 받음)
-    const question = rl.question(word, (answer)=>{
 
-        // 답을 미리 출력해줌
-        console.log(answer)
-        // 종료
+    // 시도 
+    try{
+        // 질문을 함(질문하면서 값도 받음)
+        const question = await rl.question(word, (answer)=>{
+            // 답을 미리 출력해줌
+            console.log(answer)
+        })
+        
+        // 값을 출력
+        return question
+
+    }
+
+    finally{
+        
+        // rl 닫아줌, 프로그램 끝내기
+        // 닫아주지 않으면 프로그램이 영원히 끝나지 않게 됨
         rl.close()
-    })
+
+    }
     
-    // 값을 출력
-    return question
+    
 }
 
 // export
