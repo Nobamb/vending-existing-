f0eca62548a3c0b6c2056364e0c32f61cc907e77
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-09 14:55:53 +0900

    남은 돈 알림

--- input/func/showAllUserMoney.js
+++ input/func/showAllUserMoney.js
@@ -3,6 +3,8 @@ import moneyData from "../../data/user/moneyData.js";
 
 // user의 전체 money 데이터를 보여줌
 const showAllUserMoney = () => {
+  //   실행할 때마다 유저의 전체 현금 데이터라고 알려줌
+  console.log("남은 돈 상태입니다.");
   // moneyData하나씩 출력
   moneyData.forEach((element) => {
     console.log(element);
